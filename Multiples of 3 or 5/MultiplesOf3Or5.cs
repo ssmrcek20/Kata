cb095864a1a83f49d4f2d7b15c26340da37b9f02
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-06-18 21:20:16 +0200

    Multiples of 3 or 5 Kata

--- Multiples of 3 or 5/MultiplesOf3Or5.cs
+++ Multiples of 3 or 5/MultiplesOf3Or5.cs
@@ -8,6 +8,15 @@ namespace Kata.Multiples_of_3_or_5
 {
     public class MultiplesOf3Or5
     {
-
+        public int GetValue(int number)
+        {
+            int sum = 0;
+            for(int i = 1; i < number; i++)
+            {
+                sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+            }
+            return sum;
+        }
     }
 }
+
--- Multiples of 3 or 5/MultiplesOf3Or5_Tests.cs
+++ Multiples of 3 or 5/MultiplesOf3Or5_Tests.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Kata.String_Sum_Kata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Kata.Multiples_of_3_or_5
 {
-    internal class MultiplesOf3Or5_Tests
+    public class MultiplesOf3Or5_Tests
     {
+        [Theory]
+        [InlineData(0, -10)]
+        [InlineData(23, 10)]
+        public void Test(int expValue, int number)
+        {
+            MultiplesOf3Or5 multiplesOf3Or5 = new MultiplesOf3Or5();
+
+            int actValue = multiplesOf3Or5.GetValue(number);
+
+            Assert.Equal(expValue, actValue);
+        }
     }
 }

470bd795afee71687ff7dbad5393526dfb8418a1
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-06-19 17:15:51 +0200

    Who likes it Kata

---
+++ Who likes it/WhoLikesIt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Who_likes_it
+{
+    public class WhoLikesIt
+    {
+        public string GetDisplayText(string[] names)
+        {
+            switch (names.Length)
+            {
+                case 0: return "no one likes this";
+                case 1: return names[0] + " likes this";
+                case 2: return names[0] + " and " + names[1] + " like this";
+                case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+                default: return names[0] + ", " + names[1] + " and " + GetNumberOfPeopele(names) + " others like this";
+            }
+        }
+
+        private string GetNumberOfPeopele(string[] names)
+        {
+            int number = names.Count() -2;
+            return number.ToString();
+        }
+    }
+}
---
+++ Who likes it/WhoLikesIt_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Kata.Multiples_of_3_or_5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kata.Who_likes_it
+{
+    public class WhoLikesIt_Tests
+    {
+        [Theory]
+        [InlineData("no one likes this", new string[0])]
+        [InlineData("Peter likes this", new string[] { "Peter" } )]
+        [InlineData("Jacob and Alex like this", new string[] { "Jacob", "Alex" })]
+        [InlineData("Max, John and Mark like this", new string[] { "Max", "John", "Mark" })]
+        [InlineData("Alex, Jacob and 2 others like this", new string[] { "Alex", "Jacob", "Mark", "Max" })]
+        public void Test(string expValue, string[] names)
+        {
+            WhoLikesIt whoLikesIt = new WhoLikesIt();
+
+            string actValue = whoLikesIt.GetDisplayText(names);
+
+            Assert.Equal(expValue, actValue);
+        }
+    }
+}

098ab93639c354fd5073cf2c0e3bd80599ed2ad7
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-06-20 01:06:09 +0200

    Snail Kata

---
+++ Snail/Snail.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Snail
+{
+    public class Snail
+    {
+        public int[] GetArray(int[][] array)
+        {
+            int iteration2 = 0;
+            int iteration1 = 0;
+            List<int> list = new List<int>();
+
+            if(array.Length % 2 == 0)
+            {
+                for(int i = 0; i < array[0].Length/2-1; i++)
+                {
+                    list.AddRange(Forward(iteration1, iteration2, array[i]));
+                    list.AddRange(Down(iteration1, iteration2, array));
+                    list.AddRange(Backward(iteration1, iteration2, array[array[0].Length - i - 1]));
+                    list.AddRange(Up(iteration1, iteration2, array));
+                    iteration2 += 2;
+                    iteration1 += 1;
+                }
+                list.AddRange(Forward(iteration1, iteration2, array[array[0].Length/2-1]));
+                list.AddRange(Down(iteration1, iteration2, array));
+                list.AddRange(Backward(iteration1, iteration2, array[array[0].Length/2]));
+
+                return list.ToArray();
+            }
+            else
+            {
+                for (int i = 0; i < array[0].Length/2; i++)
+                {
+                    list.AddRange(Forward(iteration1, iteration2, array[i]));
+                    list.AddRange(Down(iteration1, iteration2, array));
+                    list.AddRange(Backward(iteration1, iteration2, array[array[0].Length - i - 1]));
+                    list.AddRange(Up(iteration1, iteration2, array));
+                    iteration2 += 2;
+                    iteration1 += 1;
+                }
+                list.AddRange(Forward(iteration1, iteration2, array[array[0].Length/2]));
+
+                return list.ToArray();
+            }
+        }
+
+        private int[] Up(int iteration1, int iteration2, int[][] array)
+        {
+            int[] newArray = new int[array[0].Length - iteration2 - 2];
+            int index = 0;
+            for (int i = array[0].Length -2 - iteration1; i > iteration1; i--)
+            {
+                newArray[index] = array[i][iteration1];
+                index++;
+            }
+            return newArray;
+        }
+        private int[] Backward(int iteration1, int iteration2, int[] array)
+        {
+            int[] newArray = new int[array.Length - iteration2 - 1];
+            int index = 0;
+            for (int i = array.Length - iteration1 - 2; i >= iteration1; i--)
+            {
+                newArray[index] = array[i];
+                index++;
+            }
+            return newArray;
+        }
+
+        private int[] Down(int iteration1, int iteration2, int[][] array)
+        {
+            int[] newArray = new int[array[0].Length - iteration2 - 1];
+            for (int i = 1 + iteration1; i < array[0].Length - iteration2 + iteration1; i++)
+            {
+                newArray[i-1-iteration1] = array[i][array[0].Length - iteration1 - 1];
+            }
+            return newArray;
+        }
+
+        private int[] Forward(int iteration1, int iteration2, int[] array)
+        {
+            int[] newArray = new int[array.Length - iteration2];
+            for(int i=0 + iteration1; i < array.Length - iteration2 + iteration1; i++)
+            {
+                newArray[i-iteration1] = array[i];
+            }
+            return newArray;
+        }
+    }
+}
---
+++ Snail/Snail_test.cs
@@ -0,0 +1,47 @@
+﻿using Kata.Human_Readable_Time;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kata.Snail
+{
+    public class Snail_test
+    {
+        [Fact]
+        public void Test3()
+        {
+            Snail snail = new Snail();
+            int[] expArray = new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            int[][] array =
+            {
+                new []{1, 2, 3},
+                new []{4, 5, 6},
+                new []{7, 8, 9}
+            };
+
+            int[] actArray = snail.GetArray(array);
+
+            Assert.Equal(expArray, actArray);
+        }
+        [Fact]
+        public void Test4()
+        {
+            Snail snail = new Snail();
+            int[] expArray = new[] { 1, 2, 3, 1, 4, 7, 7, 9, 8, 7, 7, 4, 5, 6, 9, 8 };
+            int[][] array =
+            {
+                new []{1, 2, 3, 1},
+                new []{4, 5, 6, 4},
+                new []{7, 8, 9, 7},
+                new []{7, 8, 9, 7}
+            };
+
+            int[] actArray = snail.GetArray(array);
+
+            Assert.Equal(expArray, actArray);
+        }
+    }
+}

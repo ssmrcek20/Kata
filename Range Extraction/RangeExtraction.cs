966f7f281e6fbe28a1431d6d80c549c863c97ebc
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-07-13 22:08:46 +0200

    RangeExtraction v2

--- Range Extraction/RangeExtraction.cs
+++ Range Extraction/RangeExtraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,49 +12,88 @@ namespace Kata.RangeExtraction
         public string Extract(int[] args)
         {
             StringBuilder sb = new StringBuilder();
-            bool inRange = false;
-            for(int i = 1; i < args.Length-1; i++)
-            {
-                if(i > 2 && args[i-1] == args[i-2])
-                {
+            bool lastArg = false;
 
-                }
-                else if (args[i] != args[i-1]+1)
-                {
-                    sb.Append(args[i-1]);
-                    sb.Append(",");
-                }
-                else if(!inRange && args[i] == args[i+1]-1)
-                {
-                    sb.Append(args[i-1]);
-                    sb.Append("-");
-                    inRange = true;
-                }
-                else if(inRange && args[i] != args[i+1]-1)
-                {
-                    sb.Append(args[i]);
-                    sb.Append(",");
-                    inRange = false; 
-                    i++;
-                }
-                else if(!inRange)
-                {
-                    sb.Append(args[i-1]);
-                    sb.Append(",");
-                }
-            }
-            if(args.Length == 2)
+            for (int i = 0; i < args.Length-1; i++)
             {
-                sb.Append(args[0]);
-                sb.Append(",");
+                if (NoRange(args, i))
+                {
+                    AddArg(args, sb, i);
+                }
+                else if (IsRange(args, i))
+                {
+                    AddRange(args, sb, ref lastArg, ref i);
+                }
+                else AddArg(args, sb, i);
             }
-            else if (!inRange && args[args.Length-2] != args[args.Length-3])
-            {
-                sb.Append(args[args.Length-2]);
-                sb.Append(",");
-            }
-            sb.Append(args[args.Length-1]);
+            if (!lastArg) sb.Append(LastArg(args));
+
             return sb.ToString();
+        }
+
+        private static int LastArg(int[] args)
+        {
+            return args[args.Length-1];
+        }
+
+        private static void AddRange(int[] args, StringBuilder sb, ref bool lastArg, ref int i)
+        {
+            int startRange = args[i];
+            for (i +=2; i<args.Length; i++)
+            {
+                if (EndOfArgs(args, i))
+                {
+                    AddRangeArg(args, sb, i, startRange);
+                    lastArg = true;
+                    break;
+                }
+                else if (SameArg(args, i))
+                {
+                    AddRangeArg(args, sb, i, startRange);
+                    sb.Append(",");
+                    i++;
+                    break;
+                }
+                else if (NoRange(args, i))
+                {
+                    AddRangeArg(args, sb, i, startRange);
+                    sb.Append(",");
+                    break;
+                }
+            }
+        }
+
+        private static bool SameArg(int[] args, int i)
+        {
+            return args[i] == args[i+1];
+        }
+
+        private static bool EndOfArgs(int[] args, int i)
+        {
+            return args.Length == i+1;
+        }
+
+        private static bool IsRange(int[] args, int i)
+        {
+            return args.Length != i+2 && args[i+1] == args[i+2]-1;
+        }
+
+        private static bool NoRange(int[] args, int i)
+        {
+            return args[i] != args[i+1]-1;
+        }
+
+        private static void AddRangeArg(int[] args, StringBuilder sb, int i, int startRange)
+        {
+            sb.Append(startRange);
+            sb.Append("-");
+            sb.Append(args[i]);
+        }
+
+        private static void AddArg(int[] args, StringBuilder sb, int i)
+        {
+            sb.Append(args[i]);
+            sb.Append(",");
         }
     }
 }
